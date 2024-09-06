49371b74c89133469ba79572354caed77ce7d7e2
Sunho Cha <carprefer@postech.ac.kr>
2024-09-06 17:45:38 +0900

    [assn1] complete Main.scala & pass all test

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -1,6 +1,8 @@
 package recfun
 import common._
 
+import scala.annotation.tailrec
+
 object Main {
   def main(args: Array[String]) {
     println("Pascal's Triangle")
@@ -14,15 +16,36 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    if(c == 0 || c == r) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
+  }
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    @tailrec
+    def balanceR(chars: List[Char], open: Int, close: Int): Boolean = {
+      if(chars.isEmpty) open == close
+      else if(open < close) false
+      else if(chars.head == '(') balanceR(chars.tail, open + 1, close)
+      else if(chars.head == ')') balanceR(chars.tail, open, close + 1)
+      else balanceR(chars.tail, open, close)
+    }
+    balanceR(chars, 0, 0)
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    def countChangeR(money: Int, coins: List[Int]): Int = {
+      if(money == 0) 1
+      else if(coins.isEmpty) 0
+      else if(money < coins.head) countChangeR(money, coins.tail)
+      else countChangeR(money - coins.head, coins) + countChangeR(money, coins.tail)
+    }
+    countChangeR(money, coins.sortWith(_ > _))
+  }
 }
